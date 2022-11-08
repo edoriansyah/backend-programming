aba25c03aa11bc48b9218ff6f0cc4f1694943507
Edo Riansyah <edoriansyah@gmail.com>
2022-11-08 12:59:51 +0700

    Create Student show, update & destroy action

--- rest-api/app/Http/Controllers/StudentController.php
+++ rest-api/app/Http/Controllers/StudentController.php
@@ -48,4 +48,90 @@ class StudentController extends Controller
         # mengembalikan data (json) status code 201
         return response()->json($data, 201);
     }
+
+    # mendapatkan detail resource student
+    # membuat method show
+    public function show($id)
+    {
+        # cari data student
+        $student = Student::find($id);
+
+        if ($student) {
+            $data = [
+                'message' => 'Get detail student',
+                'data' => $student,
+            ];
+
+            # mengembalikan data json status code 200
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Student not found',
+            ];
+
+            # mengembalikan data json status code 404
+            return response()->json($data, 404);
+        }
+    }
+
+    # mengupdate resource student
+    # membuat method update
+    public function update(Request $request, $id)
+    {
+        # cari data student yg ingin diupdate
+        $student = Student::find($id);
+
+        if ($student) {
+            # mendapatkan data request
+            $input = [
+                'nama' => $request->nama ?? $student->nama,
+                'nim' => $request->nim ?? $student->nim,
+                'email' => $request->email ?? $student->email,
+                'jurusan' => $request->jurusan ?? $student->jurusan,
+            ];
+
+            # mengupdate data
+            $student->update($input);
+
+            $data = [
+                'message' => 'Resource student updated',
+                'data' => $student,
+            ];
+
+            # mengirimkan respon json dgn status code 200
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Student not found',
+            ];
+
+            # mengembalikan data json status code 404
+            return response()->json($data, 404);
+        }
+    }
+
+    public function destroy($id)
+    {
+        # cari data student yg ingin dihapus
+        $student = Student::find($id);
+
+        if ($student) {
+            # hapus data student
+            $student->delete();
+
+            $data = [
+                'message' => 'Student is deleted',
+            ];
+
+            # mengembalikan data json status code 200
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Student not found',
+            ];
+
+            # mengembalikan data json status code 404
+            return response()->json($data, 404);
+        }
+    }
 }
